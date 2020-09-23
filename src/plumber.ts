5f9664da5d40cc5c66c1288000e8cf50fd618825
Steve Oney <swloney@gmail.com>
2020-09-23 00:55:11 -0400

    Added op queue

--- src/plumber.ts
+++ src/plumber.ts
@@ -24,44 +24,99 @@ export const plumber = {
     }
 }
 
-type Subscriber = Function;
+type Subscriber<T> = (data: T) => any;
+
+enum PipeOperationType {
+    SEND = 'send',
+    CLOSE = 'close'
+};
+interface PipeOperation {
+    type: PipeOperationType
+}
+interface SendPipeOperation<T> extends PipeOperation {
+    type: PipeOperationType.SEND,
+    data: T
+}
+interface ClosePipeOperation extends PipeOperation {
+    type: PipeOperationType.CLOSE
+}
+
+enum PipeState {
+    CONNECTING = WebSocket.CONNECTING,
+    OPEN       = WebSocket.OPEN,
+    CLOSED     = WebSocket.CLOSED,
+    CLOSING    = WebSocket.CLOSING
+};
 
 class Pipe<T> {
     private websocket: WebSocket;
-    private subscribers: Subscriber[] = [];
+    private subscribers: Subscriber<T>[] = [];
+    private operationQueue: PipeOperation[] = [];
+    private state: PipeState = PipeState.CONNECTING;
+
     constructor(private name: string, private serverURL: string) {
         this.websocket = new WebSocket(serverURL);
-        this._doWebsocketAddEventListener('message', (event) => {
+        this.websocket.addEventListener('open', () => {
+            this.state = PipeState.OPEN;
+            this.runOperationQueue();
+        });
+        this.websocket.addEventListener('close', () => {
+            this.state = PipeState.CLOSED;
+        });
+        this.websocket.addEventListener('message', (event) => {
+            const { data } = event;
+            const parsedData = JSON.parse(data) as T;
             this.subscribers.forEach((subscriber) => {
-                subscriber(event);
+                subscriber(parsedData);
             });
         });
     }
 
-    public subscribe(subscriber: Subscriber): void { this.subscribers.push(subscriber); }
-    public unsubscribe(subscriber: Subscriber): void {
+    public subscribe(subscriber: Subscriber<T>): void {
+        this.subscribers.push(subscriber);
     }
-    protected _doWebsocketSend(data: string|ArrayBuffer|Blob|ArrayBufferView): void {
-        this.websocket.send(data);
+    public unsubscribe(subscriber: Subscriber<T>): void {
+        for(let i: number = this.subscribers.length - 1; i>=0; i--) {
+            if(subscriber === this.subscribers[i]) {
+                this.subscribers.splice(i, 1);
+            }
+        }
     }
-    protected _doWebsocketAddEventListener(eventType: 'message'|'open'|'close'|'error', listener: (this: WebSocket, ev: MessageEvent<any> | Event | CloseEvent) => any): void {
-        this.websocket.addEventListener(eventType, listener);
+
+    private enqueueOperation(op: PipeOperation): void {
+        this.operationQueue.push(op);
     }
-    protected _doWebsocketRemoveEventListener(eventType: 'message'|'open'|'close'|'error', listener: (this: WebSocket, ev: MessageEvent<any> | Event | CloseEvent) => any): void {
-        this.websocket.removeEventListener(eventType, listener);
+
+    private async runOperationQueue(): Promise<void> {
+        while(this.operationQueue.length > 0) {
+            const op = this.operationQueue.shift();
+            await this.executeOperation(op);
+        }
     }
-    protected _doWebsocketClose(code?: number, reason?: string): void {
-        this.websocket.close(code, reason);
-    }
-    protected _getWebsocketReadyState(): number {
-        return this.websocket.readyState;
+
+    private async executeOperation(op: PipeOperation): Promise<void> {
+        const { type } = op;
+        if(type === PipeOperationType.SEND) {
+            const { data } = op as SendPipeOperation<T>;
+            return this.websocket.send(JSON.stringify(data));
+        } else if (type === PipeOperationType.CLOSE) {
+            const {} = op as ClosePipeOperation;
+            this.state = PipeState.CLOSING;
+            return this.websocket.close();
+        } else {
+            throw new Error(`Unknown op type ${type}`)
+        }
     }
 
     public send(data: T): void {
-        this._doWebsocketSend(JSON.stringify(data));
+        const op: SendPipeOperation<T> = { type: PipeOperationType.SEND, data };
+        this.enqueueOperation(op);
+        if(this.state === PipeState.OPEN) { this.runOperationQueue(); }
     }
 
     public close(): void {
-        this._doWebsocketClose();
+        const op: ClosePipeOperation = { type: PipeOperationType.CLOSE };
+        this.enqueueOperation(op);
+        if(this.state === PipeState.OPEN) { this.runOperationQueue(); }
     }
 }
